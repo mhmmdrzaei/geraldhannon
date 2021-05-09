d720229ef096daa05a17cfdec8328966d74d318d
Mohammad <mhmmdrzaei@gmail.com>
2021-05-09 00:24:17 -0400

    books

--- page-book.php
+++ page-book.php
@@ -1,28 +1,41 @@
 <?php get_header();  ?>
-
-      <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <?php the_post_thumbnail(); ?>
-        <?php the_field('book_details'); ?>
-        <?php if( have_rows('reviews') ): ?>
-            <section class="reviewsSide">
-            <?php while( have_rows('reviews') ): the_row(); ?>
-                <div>
-                  <?php the_sub_field('review_each'); ?>
-                </div>
-            <?php endwhile; ?>
+<main>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <section class="pageBookContent">
+        <section class="pageBookside">
+            <figure>
+               <?php the_post_thumbnail('large'); ?> 
+            </figure>
+            <section class="bookDetails">
+                <?php the_field('book_details'); ?>
             </section>
-        <?php endif; ?>
-        <?php if( have_rows('book_links') ): ?>
-            <ul class="salesLinks">
-            <?php while( have_rows('book_links') ): the_row(); ?>
-              <li><a href="<?php the_sub_field('link_address_book') ;?>"><?php the_sub_field('link_label_book'); ?></a></li>
-            <?php endwhile; ?>
-            </ul>
-        <?php endif; ?>
+            <?php if( have_rows('reviews') ): ?>
+                <section class="reviewsSide">
+                <?php while( have_rows('reviews') ): the_row(); ?>
+                    <div class="reviewsEach">
+                      <?php the_sub_field('review_each'); ?>
+                    </div>
+                <?php endwhile; ?>
+                </section>
+            <?php endif; ?>
+            <?php if( have_rows('book_links') ): ?>
+                <ul class="salesLinks">
+                <?php while( have_rows('book_links') ): the_row(); ?>
+                  <li><a href="<?php the_sub_field('link_address_book') ;?>"><?php the_sub_field('link_label_book'); ?></a></li>
+                <?php endwhile; ?>
+                </ul>
+            <?php endif; ?>
 
-
-        <?php the_content(); ?>
-      <?php endwhile; endif; ?>
+        </section>
+        <section class="pageBookMain">
+            <h2><a href="#"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+        </section>
+        <?php endwhile; endif; ?>
+    </section>     
 <?php wp_reset_query();?> 
 
 <?php get_footer(); ?>
+    
+</main>
+
